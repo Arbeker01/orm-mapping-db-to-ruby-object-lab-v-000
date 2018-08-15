a8d06ddf4639cca9c79631068817e83f75c0c6cf
Romeo Gono <romeogono1@aol.com>
2018-08-15 01:04:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,5 +63,12 @@ class Student
     SQL
     DB[:conn].execute(sql).map {|row| self.new_from_db(row)}
   end
-
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade < 12
+    SQL
+    DB[:conn].execute(sql).map {|row| self.new_from_db(row)}
+  end
 end
